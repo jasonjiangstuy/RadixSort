ae46a45a82759f27de938e4bdf5ef534e4a0503d
jasonjiangstuy <jasonjiangny@gmail.com>
2021-01-13 05:56:59 -0500

    implemented and tested radix sort simple and regular

--- Radix.java
+++ Radix.java
@@ -1,7 +1,7 @@
 import java.lang.Math;
 public class Radix{
     public static int nth(int n, int col){
-
+        n = Math.abs(n);
         // nth(123,1) -> 2
         double temp = (double)n % Math.pow(10 ,col + 1);
         // System.out.println(temp);
@@ -32,4 +32,52 @@ public class Radix{
             }
         } 
     }
+    // 5. Write a method that sorts non-negative integer values: [This part is the most important part, and I expect every student can complete it!]
+    public static void radixSortSimple(SortableLinkedList data){
+        int pointerCol = 0;
+        while (radixpass(data, pointerCol)){
+            pointerCol++;
+        }
+    }
+    public static void radixSort(SortableLinkedList data){
+        int pointerCol = 0;
+        while (radixpass(data, pointerCol)){
+            pointerCol++;
+        }
+    }
+    public static boolean radixpass(SortableLinkedList data, int col){
+        SortableLinkedList[] buckets = new SortableLinkedList[]{new SortableLinkedList(), new SortableLinkedList(), 
+            new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList(), 
+            new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList(), 
+            new SortableLinkedList(), new SortableLinkedList(), 
+            new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList(), 
+            new SortableLinkedList(), new SortableLinkedList(), new SortableLinkedList() ,new SortableLinkedList()
+        };
+
+        boolean goNext = false;
+        while (data.size() != 0){
+            if (length(data.get(0)) > col){
+                goNext = true;
+            }
+            
+            if (data.get(0) < 0){
+                buckets[9 - nth(data.get(0), col)].add(data.get(0));
+                // System.out.println(9 - nth(data.get(0), col));
+            }else{
+                buckets[nth(data.get(0), col) + 9].add(data.get(0));
+            }
+            
+            data.remove(0);
+            
+        }
+        // data should be empty
+
+
+        merge(data, buckets);
+        
+        return (goNext);
+    }
+
+    // Assume there are no negative values.
+    // Use the algorithm described in class/class notes
 } 
--- Tester.java
+++ Tester.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 public class Tester{
     public static void main(String[] args) {
         System.out.println("Test Nth");
@@ -6,7 +6,12 @@ public class Tester{
         tester("nth", 123, 1,  2);
         tester("nth", 123, 2,  1);
         tester("nth", 123, 0,  3);
+        tester("nth", 123, 3,  0);
 
+        tester("nth", -123, 1,  2);
+        tester("nth", -123, 2,  1);
+        tester("nth", -123, 0,  3);
+        tester("nth", -123, 3,  0);
         System.out.println("Test length");
         tester("length", 0, 1);
         tester("length", 15, 2);
@@ -54,6 +59,24 @@ public class Tester{
 
         // should work on updating empty MyLinkedList
         tester("mergeSorted", myTestLinkedListS, new SortableLinkedList[] {mergeS, merge1S, merge2S}, "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+
+        System.out.println("Test radixSortSimple");
+        SortableLinkedList unsorted = new SortableLinkedList();
+        int[] testArray = new int[]{12, 34, 42, 32, 44, 41, 34, 11, 32, 23, 87, 50, 77, 58, 8};
+        for (int i : testArray){
+            unsorted.add(i);
+        }
+        Arrays.sort(testArray);
+        tester("radixSortSimple", unsorted, Arrays.toString(testArray));
+
+        System.out.println("Test radixSort");
+        unsorted = new SortableLinkedList();
+        testArray = new int[]{12, 34, 42, 32, 44, -41, 34, 11, 32, 23, -87, 50, 77, -58, 8};
+        for (int i : testArray){
+            unsorted.add(i);
+        }
+        Arrays.sort(testArray);
+        tester("radixSort", unsorted, Arrays.toString(testArray));
     }
     
 
@@ -76,6 +99,19 @@ public class Tester{
             // System.out.println(Radix.length((int)values[1]));
             Radix.merge((SortableLinkedList)values[1], (SortableLinkedList[])values[2]);
             System.out.println(values[1].toString().equals(values[3]));
+        }
+        if (values[0].equals("radixSortSimple")){
+            // System.out.println(Radix.length((int)values[1]));
+            Radix.radixSortSimple((SortableLinkedList)values[1]);
+            // System.out.println(values[1].toString() + "\n"+ values[2]);
+            // System.out.println(values[1].toString().equals(values[2]));
+        }
+
+        if (values[0].equals("radixSort")){
+            // System.out.println(Radix.length((int)values[1]));
+            Radix.radixSort((SortableLinkedList)values[1]);
+            // System.out.println(values[1].toString() + "\n"+ values[2]);
+            // System.out.println(values[1].toString().equals(values[2]));
         }
 
 
